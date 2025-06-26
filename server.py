ed5ed34ff0cd31ea027178f98b54200042698fe9
Hazz-i <wahidh697@gmail.com>
2025-06-26 13:32:57 +0700

    update: cross

--- server.py
+++ server.py
@@ -34,7 +34,8 @@ app.add_middleware(
     allow_origins=[
         os.getenv("URL", "http://localhost:5173"), 
         "http://127.0.0.1:5173",
-        "http://localhost:5173"
+        "http://localhost:5173",
+        "https://comot-in.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
